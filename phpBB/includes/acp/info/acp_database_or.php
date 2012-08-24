c847caa7e4f80b326258e0d6abd5eeabac260e95
Diskmaster <diskmaster.aviary@gmail.com>
2012-08-24 02:51:11 -0500

    Database ACP Optimize / Repair toolkit (1.0.1)

---
+++ phpBB/includes/acp/info/acp_database_or.php
@@ -0,0 +1,45 @@
+<?php
+/**
+*
+* @package acp
+* @version $Id: acp_database_or.php 10 11/22/10 9:15 AM VSE $
+* @copyright (c) 2010 Matt Friedman
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License
+*
+*/
+
+/**
+ * @ignore
+ */
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+/**
+* @package module_install
+*/
+class acp_database_or_info
+{
+	function module()
+	{
+		return array(
+			'filename'	=> 'acp_database_or',
+			'title'		=> 'ACP_DATABASE_OR',
+			'version'	=> '1.0.1',
+			'modes'		=> array(
+				'view'	=> array('title' => 'ACP_DATABASE_OR', 'auth' => 'acl_a_backup', 'cat' => array('ACP_CAT_DATABASE')),
+			),
+		);
+	}
+
+	function install()
+	{
+	}
+
+	function uninstall()
+	{
+	}
+}
+
+?>
